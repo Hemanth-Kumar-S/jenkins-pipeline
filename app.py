53e98da507a8426e93aa71c5518b51b9cb33ac1a
hemanth <hemanthachar007@gmail.com>
2026-01-29 17:48:26 +0000

    latest added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from hemss 🚀"
+    return "Hello from hemanth 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
